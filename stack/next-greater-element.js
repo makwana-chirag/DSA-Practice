5428140a195193b9d6beba2dfb8660b552b67d1e
am.chirag.pm@gmail.com <am.chirag.pm@gmail.com>
2026-09-16 15:16:03 +0530

    stack problem added

--- stack/next-greater-element.js
+++ stack/next-greater-element.js
@@ -1,6 +1,6 @@
 // brute-force approach without stack 
 
-const array = [2,5,4,10,8];
+// const array = [2,5,4,10,8];
 
 const nextGreaterBruteForce  = (arr) => {
 
@@ -17,4 +17,28 @@ const nextGreaterBruteForce  = (arr) => {
     }
 }
 
-console.log(nextGreaterBruteForce(array))
+// console.log(nextGreaterBruteForce(array))
+
+// next greater element with stack (monotonic)
+
+const array = [2,5,4,3,10];
+
+const nextGreaterElement = (arr) => {
+
+    const answer = new Array(arr.length).fill(-1);
+    const stack = [];
+
+
+    for (let i = 0; i < arr.length ; i++) {
+        while(stack.length > 0 && arr[i] > arr[stack[stack.length -1]]){
+             answer[stack[stack.length - 1]] = arr[i]
+             stack.pop()
+        }
+        stack.push(i)
+    }
+
+    return answer;
+
+}
+
+console.log(nextGreaterElement(array))
